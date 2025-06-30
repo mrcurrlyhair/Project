f2c6e2ecd3b20e1b3ef1a778284852cdcdb3a568
mrcurrlyhair <mrcurrlyhair@users.noreply.github.com>
2025-06-30 20:11:02 +0100

    Healthdb create update
    updated health db with the correct columns

--- create_healthdb.py
+++ create_healthdb.py
@@ -4,14 +4,22 @@ conn = sqlite3.connect('health.db')
 cursor = conn.cursor()
 
 
-# TESTING 
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS health (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         user_id INTEGER UNIQUE,
         age INTEGER,
-        weight REAL,
-        blood_type TEXT,
+        gender TEXT,
+        postcode TEXT,
+        smoking_status TEXT,
+        BMI REAL,
+        alcohol_use TEXT,
+        physical_activity TEXT,
+        diet_quality TEXT,
+        sleep_hours REAL,
+        county_name TEXT,
+        radon_level INTEGER,
+        pollution REAL,
         FOREIGN KEY (user_id) REFERENCES users(id)
     )
 ''')
---
+++ models_test.py
@@ -0,0 +1,1 @@
+# create models and test 
