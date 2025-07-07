7acc7c852b49494fb2d0746968a4e4d39cbdcbd1
mrcurrlyhair <mrcurrlyhair@users.noreply.github.com>
2025-07-07 18:08:20 +0100

    Merge database creation
    merge the creation of health and user dbs

--- create_dbs.py
+++ create_dbs.py
@@ -1,7 +1,24 @@
 import sqlite3
 
-conn = sqlite3.connect('health.db')
-cursor = conn.cursor()
+con = sqlite3.connect('users.db')
+cursor = con.cursor()
+
+# create user db
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
+    )
+''')
+
+con.commit()
+con.close()
+print("User database created")
+
+
+con = sqlite3.connect('health.db')
+cursor = con.cursor()
 
 # create health db 
 cursor.execute('''
@@ -20,11 +37,11 @@ cursor.execute('''
         diet_quality TEXT,
         sleep_hours REAL,
         radon_level INTEGER,
-        pollution REAL,
+        pollution_level REAL,
         FOREIGN KEY (user_id) REFERENCES users(id)
     )
 ''')
 
-conn.commit()
-conn.close()
+con.commit()
+con.close()
 print("Health database created")
