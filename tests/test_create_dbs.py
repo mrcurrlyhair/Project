4cb0f6abdf49ce5f90ae263b1c48637355cdefdb
mrcurrlyhair <mrcurrlyhair@users.noreply.github.com>
2025-07-22 17:15:07 +0100

    Test DBs
    Creates one test to make sure the dbs files are created

--- tests/test_create_dbs.py
+++ tests/test_create_dbs.py
@@ -1,1 +1,15 @@
 import pytest
+import os 
+import sqlite3
+
+# test if users.db and health.db are created
+def test_db_files_exist():
+    os.system('python create_dbs.py')
+    assert os.path.exists('users.db'), "users.db not found"
+    assert os.path.exists('health.db'), "health.db not found"
+
+    # remove the dbs after test
+    os.remove('users.db')
+    os.remove('health.db')
+
+
