dd540a911c3a46d23906f122d8adb4fa6fb834cf
mrcurrlyhair <mrcurrlyhair@users.noreply.github.com>
2025-07-22 17:00:19 +0100

    Comments test_gen_key
    Comments added for test_gen_key

--- tests/test_gen_key.py
+++ tests/test_gen_key.py
@@ -3,12 +3,13 @@ import os
 from cryptography.fernet import Fernet
 
 
-
+# test to see if the encryption key file exists 
 def test_key_file_exists():
     os.system('python gen_key.py')
     assert os.path.exists('encryption.key')
     os.remove('encryption.key')
 
+# test to see if the encryption key is the correct length (44 characters, fernet)
 def test_key_length():
     os.system('python gen_key.py')
     with open('encryption.key', 'rb') as f:
