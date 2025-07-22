5472b58b32116a1e0f65d15ed1aace0598c285a3
mrcurrlyhair <mrcurrlyhair@users.noreply.github.com>
2025-07-22 18:28:15 +0100

    Comments

--- tests/test_clean_pollution.py
+++ tests/test_clean_pollution.py
@@ -5,11 +5,14 @@ import pandas as pd
 # test clean_pollution.py output
 def test_clean_pollution():
     os.system('python clean_pollution.py')
+
+    # check if the file exists 
     assert os.path.exists('CSVs/clean_pollution.csv')
 
+    # load CSV 
     df = pd.read_csv('CSVs/clean_pollution.csv')
 
-    # 108 counties check
+    # 108 counties check with no missing pollution levels
     assert len(df) == 108
     assert df['pollution_level'].notnull().all()
 
--- tests/test_gen_key.py
+++ tests/test_gen_key.py
@@ -6,6 +6,8 @@ from cryptography.fernet import Fernet
 # test to see if the encryption key file exists 
 def test_key_file_exists():
     os.system('python gen_key.py')
+
+    # check if the file exists
     assert os.path.exists('encryption.key')
 
     # remove the encrpytion key after test
@@ -14,6 +16,7 @@ def test_key_file_exists():
 # test to see if the encryption key is the correct length (44 characters, fernet)
 def test_key_length():
     os.system('python gen_key.py')
+    
     with open('encryption.key', 'rb') as f:
         key = f.read()
     assert len(key) == 44
