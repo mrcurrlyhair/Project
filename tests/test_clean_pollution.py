de7b3c3f616b93867781f7a77b6801813a998434
mrcurrlyhair <mrcurrlyhair@users.noreply.github.com>
2025-07-22 18:13:13 +0100

    Test clean_pollution added
    Test clean_pollution added, checks if 108 counties are in the csv file and also if the file is created or not

--- tests/test_clean_pollution.py
+++ tests/test_clean_pollution.py
@@ -1,1 +1,17 @@
 import pytest
+import os
+import pandas as pd
+
+# test clean_pollution.py output
+def test_clean_pollution():
+    os.system('python clean_pollution.py')
+    assert os.path.exists('CSVs/clean_pollution.csv')
+
+    df = pd.read_csv('CSVs/clean_pollution.csv')
+
+    # 108 counties check
+    assert len(df) == 108
+    assert df['pollution_level'].notnull().all()
+
+    # delete clean_pollution after test 
+    os.remove('CSVs/clean_pollution.csv')
